59d3b3aa2ccf649493fd857f0908f35ac4f5d8c3
rajanjamu <rajanjamu@gmail.com>
2020-04-05 21:43:28 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.listen(3000, () => console.log('Yelpcamp Server has Started!'));
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (res, req) => {
+    res.send("Homepage");
+});
